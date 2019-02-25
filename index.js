8e31349842cd51a5c592b7f6a850a8fa435e41cc
Karen King <kk99807@gmail.com>
2019-02-25 12:38:32 -0800

    Setup initial node server and HelloWorld mapList endpoint - Installed express & cors (for headers) - Installed nodemon for dev - Added .gitignore - Added npm start script - Setup initial server in index.js (localhost:9000) - Setup HelloWorld at /api/mapList

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 9000;
+const HOST = process.env.HOST || 'localhost';
+const ENV = process.env.NODE_ENV || 'development';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+
+// API
+app.get('/api/mapList', (req, res) => {
+    res.send('Hello MapList');
+});
+
+// Listen
+app.listen(PORT, HOST, () => {
+    console.log('Server running on ' + HOST + ':' + PORT);
+}).on('error', (err) => {
+    console.log('Server listen error.  You probably already have a server on port: ', PORT);
+});
