0420f119b8e7ead23af78eb618d798aeb95e321c
Charu Benjwal <charubenjwal04@gmail.com>
2019-02-27 15:40:16 -0800

    adding image in casedetails page

--- client/src/components/caseDetails.js
+++ client/src/components/caseDetails.js
@@ -1,30 +1,35 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import Image from '../images';
 
 class CaseDetails extends Component {
 
     state = {
-        success: false,
         data: []
-
     }
 
     async componentDidMount() {
-        const resp = await axios.get('http://localhost:9000/api/caselist');
+        const resp = await axios.get('http://localhost:9000/api/casedetails');
         console.log('response:', resp);
 
-        this.setState({
-            success: true,
-            data: response.data.data
+        const { description ,images , size } = resp;
 
+        this.setState({
+            data: response.data
         })
 
     }
 
     render() {
+
+
         return (
             <div>
-                LIST
+                <h4>{size}</h4>
+                <div className ="product-image">
+
+                    <img src = />
+                </div>
             </div>
         );
 
