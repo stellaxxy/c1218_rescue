730ec18ed36258547f0ce5fdfed6d40ca68061a8
stellaxxy <40218939+stellaxxy@users.noreply.github.com>
2019-02-27 12:06:57 -0800

    Update caselist.js

--- client/src/components/caselist.js
+++ client/src/components/caselist.js
@@ -1,1 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 
+class CaseList extends Component {
+    state = {
+        cases: []
+    };
+
+    async componentDidMount(){
+        const result = await axios.get('/api/caselist');
+        //console.log('data from caselist:', data);
+        this.setState({
+            cases: result.data.data
+        })
+    }
+
+    render(){
+        if(this.state.cases.length === 0){
+            return(
+                <div>Loading</div>
+            );
+        }
+        const { coverImg } = this.state.cases[0];
+
+        return(
+            <div className="container">
+                <div>
+                    <img src={'./components/assets/images/cover1.jpg'}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CaseList;
