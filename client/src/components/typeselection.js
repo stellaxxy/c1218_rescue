e7f7439cc3bc78492da63d1dd957d227a64f8f36
stellaxxy <stellaxxy0109@gmail.com>
2019-02-28 16:03:23 -0800

    completed radio buttons for size selection and type selection

--- client/src/components/typeselection.js
+++ client/src/components/typeselection.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import dogImage from '../assets/images/image8.jpg';
-import catImage from "../assets/images/image6.jpg";
+import React, {Component} from 'react';
+import dogImage from '../assets/images/image9.png';
+import catImage from "../assets/images/image13.png";
 import otherAnimalImage from "../assets/images/image7.png";
 import '../assets/css/typeselection.scss';
 import NavButton from "./general/navbutton";
+import { Link } from 'react-router-dom';
 
-export default () => {
-    return (
-        <div className="typeSelectionContainer">
-            <h1>What Type of Pet?</h1>
-            <div>
-                <img src={dogImage}/>
-                <button>Dog</button>
-            </div>
-            <div>
-                <img src={catImage}/>
-                <button>Cat</button>
-            </div>
-            <div>
-                <img src={otherAnimalImage}/>
-                <button>Others</button>
-            </div>
+class TypeSelection extends Component {
+    state = {
+      selection: ''
+    };
 
-            <NavButton firstButton={{'Go Back': "/lostlanding"}} secondButton={{'Next': "/sizeselection"}} className="btn"/>
-        </div>
-    );
+    saveSelection = () => {
+        const selection= document.querySelector('input[name=typeselection]:checked').value;
+
+        this.setState({
+            selection
+        });
+
+    };
+
+    render(){
+        console.log('selection:', this.state.selection);
+        return (
+
+            <div className="typeSelectionContainer">
+                <h1>What Type of Pet?</h1>
+                <label>
+                    <input type="radio" name="typeselection" value="dog"/>
+                    <img src={dogImage}/>
+                </label>
+                <label>
+                    <input type="radio" name="typeselection" value="cat"/>
+                    <img src={catImage}/>
+                </label>
+                <label>
+                    <input type="radio" name="typeselection" value="other"/>
+                    <img src={otherAnimalImage}/>
+                </label>
+
+                <Link to="/lostlanding" className="btn">GO BACK</Link>
+                <Link to="/sizeselection" onClick={this.saveSelection} className="btn">Next</Link>
+            </div>
+        );
+    }
 }
+export default TypeSelection;
+// <NavButton firstButton={{'Go Back': "/lostlanding"}} secondButton={{'Next': "/sizeselection"}} className="btn"/>
+//
