5d78d022818ea8a58aa8cf92ba0a766317d4b027
stellaxxy <stellaxxy0109@gmail.com>
2019-03-01 18:03:53 -0800

    added redux to type selection and size selection

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -1,18 +1,29 @@
 import '../assets/css/landing.scss';
-import React from 'react';
+import React, { Component } from 'react';
 import landingImage from '../assets/images/landing-page-pet.jpg';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setActiveCaseType } from "../actions";
 
-export default () => {
-    return(
-        <div className="landing">
-            <img className="responsive-img" src={landingImage} alt=""/>
-            <p className="landing-title">Getting Pets Home</p>
-            <p className="landing-text">Search for a lost pet or list one found.</p>
-            <div className="btn-container">
-                <Link to="/lostlanding" className="waves-effect waves-light btn">Lost a Pet</Link>
-                <Link to="/foundlanding" className="waves-effect waves-light btn">Found a Pet</Link>
+class Landing extends Component {
+
+    render(){
+        return(
+            <div className="landing">
+                <img className="responsive-img" src={landingImage} alt=""/>
+                <p className="landing-title">Getting Pets Home</p>
+                <p className="landing-text">Search for a lost pet or list one found.</p>
+                <div className="btn-container">
+                    <Link onClick={()=>{this.props.setActiveCaseType('lost')}} to="/lostlanding" className="waves-effect waves-light btn">Lost a Pet</Link>
+                    <Link onClick={()=>{this.props.setActiveCaseType('found')}} to="/foundlanding" className="waves-effect waves-light btn">Found a Pet</Link>
+                </div>
             </div>
-        </div>
-    );
+        );    }
+
 }
+
+function mapStateToProps(state){
+    return {};
+}
+
+export default connect(mapStateToProps, {setActiveCaseType})(Landing);
