5b299c8dce0534185458913216ede05c9e8cf3e5
Charu Benjwal <charubenjwal04@gmail.com>
2019-03-03 22:11:22 -0800

    modal

--- client/src/components/filter.js
+++ client/src/components/filter.js
@@ -1,24 +1,70 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
 
 
 class Filter extends Component {
+
+    // state={
+    //     //     show:false
+    //     // }
+    //     //
+    //     // showModal=()=>{
+    //     //     this.setState({
+    //     //         show:true
+    //     //     })
+    //     // }
+    //     //
+    //     // hideModal=()=>{
+    //     //     this.setState({
+    //     //         show:false
+    //     //     })
+    //     // }
+
+
+    componentDidMount(){
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.modal');
+            var instances = M.Modal.init(elems, options);
+
+    });
+    }
     render() {
         return (
             <div>
                 <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
                 <div id="modal1" className="modal">
                     <div className="modal-content">
-                        <h4> Modal Header </h4>
+                        <h4>Modal Header</h4>
                         <p>A bunch of text</p>
                     </div>
                     <div className="modal-footer">
-                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Search</a>
+                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Cancel</a>
                     </div>
                 </div>
+                </main>
+
             </div>
 
-        )
+
+        );
     }
 }
 
 export default Filter
+
+
+/*
+
+    <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+                <div id="modal1" className="modal">
+                    <div className="modal-content">
+                        <h4>Modal Header</h4>
+                        <p>A bunch of text</p>
+                    </div>
+                    <div className="modal-footer">
+                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Search</a>
+                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">Cancel</a>
+                    </div>
+                </div>
+                */
