18588ef2ea46b6d6318aff1eb37575dc4bcd4395
Charu Benjwal <charubenjwal04@gmail.com>
2019-03-07 16:55:37 -0800

    created end point for closed case> changed map file for filter

--- client/src/components/casemap.js
+++ client/src/components/casemap.js
@@ -4,10 +4,17 @@ import axios from 'axios';
 import config from '../../../config/api';
 import foundIcon from '../assets/images/icons8-region-filled-48.png';
 import lostIcon from '../assets/images/icons8-region-48.png';
-import { connect } from 'react-redux';
+import FilterPanel from './case-filter';
+import connect from "react-redux/es/connect/connect";
 
 
 class CaseMap extends Component {
+
+    state = {
+        error: false,
+        cases: [],
+        filters: []
+    };
 
     componentDidMount() {
         window.initMap = this.initMap.bind(this);
@@ -15,13 +22,56 @@ class CaseMap extends Component {
         //loadJS('../googleMapClustering/markerclusterer.js');
     }
 
-    async renderMarkers(map) {
+    clean(obj) {
+        for (var propName in obj) {
+            if (obj[propName] === null || obj[propName] === undefined) {
+                delete obj[propName];
+            }
+        }
+        return Object.keys(obj).length==0 ? null : obj
+    }
+
+    async renderMarkers(params) {
+
         const icons = {
             found: foundIcon,
             lost: lostIcon
         };
 
-        const result = await axios.get('/api/caselist');
+        let filterValues = Object.assign({}, this.props.filterValues);
+
+        filterValues = this.clean(filterValues)
+
+        let filters = this.state.filters
+        if (params && params.remove) {
+            var keys = Object.keys(params.remove)
+            keys.forEach((key) => {
+                delete filterValues[key]
+                delete filters[key]
+            })
+        }
+        console.log('clean:', filterValues);
+        console.log('params', params);
+        console.log('props' , this.props);
+
+
+        const {caseType, animalType, size, zipcode, city} = filterValues || params || this.props;
+        const queryStringArray = [{caseType}, {animalType}, {size}, {zipcode}, {city}];
+
+        let endpointString = '/api/caselist?';
+
+        queryStringArray.map(item => {
+            if (Object.values(item)[0]) {
+                if (endpointString[endpointString.length - 1] !== '?') {
+                    endpointString = endpointString + '&';
+                }
+                endpointString = endpointString + Object.keys(item)[0] + '=' + Object.values(item)[0];
+                filters.push(Object.keys(item)[0])
+            }
+        });
+
+
+        const result = await axios.get(endpointString);
 
         if (result.data.success === true) {
             const markers = result.data.data.map(item => {
@@ -31,8 +81,12 @@ class CaseMap extends Component {
                 let marker = new google.maps.Marker({
                     position: new google.maps.LatLng(latitude, longitude),
                     icon: icons[item.caseType],
-                    map: map
+                    map: this.renderedMap
                 });
+
+
+                // To add the marker to the map, call setMap();
+                marker.setMap(this.renderedMap);
 
                 const img = item.coverImg;
                 let contentString = '';
@@ -49,20 +103,20 @@ class CaseMap extends Component {
                 });
 
                 marker.addListener('click', () => {
-                    infowindow.open(map, marker);
+                    infowindow.open(this.renderedMap, marker);
                 })
 
                 return marker;
             });
 
-            const markerCluster = new MarkerClusterer(map, markers,
+            const markerCluster = new MarkerClusterer(this.renderedMap, markers,
                 {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
             //'../googleMapClustering/m'
         }
     }
 
     initMap() {
-        var renderedMap = new google.maps.Map(document.getElementById("map"), {
+         this.renderedMap = new google.maps.Map(document.getElementById("map"), {
 
             center: {
                 lat: 33.6846,
@@ -72,12 +126,20 @@ class CaseMap extends Component {
             gestureHandling: 'greedy'
         });
 
-        this.renderMarkers(renderedMap);
+        this.renderMarkers();
+    }
+
+    applyFilter(params) {
+        console.log('casemap: ',params);
+        this.renderMarkers(params);
+
+
     }
 
     render() {
         return (
             <div>
+                <FilterPanel applyFilter={this.applyFilter.bind(this)}/>
                 <div id="map">
                 </div>
 
@@ -95,6 +157,13 @@ function loadJS(src) {
 }
 
 
-export default CaseMap;
+function mapStateToProps(state) {
+    return {
+        filterValues: state.caseFilters.values
+    }
+}
+
+export default connect(mapStateToProps)(CaseMap);
+
 
 
