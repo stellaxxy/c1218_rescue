1dd0f8d87b8cdc6c110122e64da406f81156cdeb
Karen King <kk99807@gmail.com>
2019-03-11 08:00:20 -0700

    Make dropzone preview image responsive

--- client/src/components/general/dropzone/image_preview.js
+++ client/src/components/general/dropzone/image_preview.js
@@ -8,7 +8,7 @@ const ImagePreview = ({ imagefile }) =>
   imagefile.map(({ name, preview, size }) => (
     <ul key={name} className="render-preview">
       <li className="image-container">
-        <img className="uploadImg" src={preview} alt={name} />
+        <img className="uploadImg" src={preview} alt={name} className="responsive-img" />
       </li>
       <li className="details">
         {name} - {size} bytes
