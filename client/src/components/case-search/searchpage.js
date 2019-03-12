e55d24d276cf5813aefc0724b93e53e98a56d81f
stellaxxy <stellaxxy0109@gmail.com>
2019-03-12 12:41:05 -0700

    added form html for search page

--- client/src/components/case-search/searchpage.js
+++ client/src/components/case-search/searchpage.js
@@ -4,6 +4,7 @@ import queryString from 'query-string';
 import SearchPanel from './searchpanel';
 import CaseList from '../cases';
 import CaseMap from '../casemap';
+import './searchpage.scss'
 
 class SearchPage extends Component {
     constructor(props) {
@@ -79,11 +80,43 @@ class SearchPage extends Component {
         } else {
             return(
                 <div className="bottomContainer">
-                    <SearchPanel filterValues={filterValues} onFilterClick={this.handleFilterClick} onFilterChange={this.handleFilterChange}/>
+                    <form className="listForm">
+                        <h5>Case Type:</h5>
+                        <div className="radioFound">
+                            <label htmlFor="found">Found</label>
+                            <input className="radioBtn" type="radio" name="caseType" value="found"/>
+                        </div>
+                        <div className="radioLost">
+                            <label htmlFor="lost">Lost</label>
+                            <input className="radioBtn" type="radio" name="caseType" value="lost"/>
+                        </div>
+                        <div className="locationDiv">
+                            <h5>Location</h5>
+                            <h6>City, Zip Code or Address</h6>
+                            <input type="text" placeholder="Irvine"/>
+                        </div>
+                        <div className="animalDiv">
+                            <h5>Animal</h5>
+                            <h6>Type</h6>
+                            <select className="selectOpt" name="animalType">
+                                <option value="dog">Dog</option>
+                                <option value="cat">Cat</option>
+                                <option value="others">Others</option>
+                            </select>
+                            <h6>Size</h6>
+                            <select className="selectOpt" name="animalSize">
+                                <option value="small">Small</option>
+                                <option value="medium">Medium</option>
+                                <option value="large">Large</option>
+                            </select>
+                        </div>
+                    </form>
+
                     <CaseList cases={this.state.cases} filterValues={filterValues}/>
                 </div>
             );
         }
+        //<SearchPanel filterValues={filterValues} onFilterClick={this.handleFilterClick} onFilterChange={this.handleFilterChange}/>
     }
 }
 
