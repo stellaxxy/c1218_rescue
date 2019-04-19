75f0294e61316d286c72cec209ae026aaa87fec9
Karen King <kk99807@gmail.com>
2019-04-19 07:21:04 -0700

    Made upload form a React Component & added Materialize DatePicker

--- client/src/components/case-upload/upload_form.js
+++ client/src/components/case-upload/upload_form.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 import DropZoneField from '../general/dropzone/dropzone_field';
@@ -6,115 +6,122 @@ import InputField from '../general/input-field';
 import {isRequired} from '../../helpers/validate';
 import './upload.scss';
 
-let UploadForm = props => {
-  const { handleSubmit, isUpdate, onSubmit, onReturn, id } = props;
+class UploadForm extends Component {
+  componentDidMount() {
+    var elems = document.querySelectorAll('input[name="caseDate"]');
+    var instances = M.Datepicker.init(elems, {format: 'mm/dd/yyyy'});
+  }
 
-  return (
-      <div className="page-body">
-        <main>
-          <div className="container">
-            <h5>Pet Details</h5>
+  render() {
+    const { handleSubmit, isUpdate, onSubmit, onDrop, onReturn, imageFile, id } = this.props;
 
-            <form id="uploadform" autoComplete="off" onSubmit={handleSubmit}>
+    return (
+        <div className="page-body">
+          <main>
+            <div className="container">
+              <h5>Pet Details</h5>
 
-            <Field
-              name="coverImg"
-              component={DropZoneField}
-              type="file"
-              imagefile={props.imageFile}
-              handleOnDrop={props.onDrop}
-              validate={[isRequired]}
-              />
+              <form id="uploadform" autoComplete="off" onSubmit={handleSubmit}>
 
-              <div>
-                <label>Case Type*</label>
-                <div className="row">
-                  <label className="col s6"><Field name="caseType" component="input" type="radio" value="lost"/><span>Lost Animal</span></label>
-                  <label className="col s6"><Field name="caseType" component="input" type="radio" value="found"/><span>Found Animal</span></label>
-                </div>
-              </div>
+              <Field
+                name="coverImg"
+                component={DropZoneField}
+                type="file"
+                imagefile={imageFile}
+                handleOnDrop={onDrop}
+                validate={[isRequired]}
+                />
 
-              <div>
-                <label>Animal Type*</label>
-                <div className="row">
-                  <label className="col s4"><Field name="animalType" component="input" type="radio" value="cat"/><span>Cat</span></label>
-                  <label className="col s4"><Field name="animalType" component="input" type="radio" value="dog"/><span>Dog</span></label>
-                  <label className="col s4"><Field name="animalType" component="input" type="radio" value="other"/><span>Other</span></label>
-                </div>
-              </div>
-
-              <div>
-                <label>Animal Size*</label>
-                <div className="row">
-                  <label className="col s4"><Field name="animalSize" component="input" type="radio" value="small"/><span>Small</span></label>
-                  <label className="col s4"><Field name="animalSize" component="input" type="radio" value="medium"/><span>Medium</span></label>
-                  <label className="col s4"><Field name="animalSize" component="input" type="radio" value="large"/><span>Large</span></label>
-                </div>
-              </div>
-
-              <div>
-                <label htmlFor="street">Location Pet Last Seen*</label>
-                <Field name="street" component={InputField} validate={isRequired} type="text" placeholder="Street address or cross streets"/>
-              </div>
-
-              <div>
-                <label htmlFor="city">City Pet Last Seen*</label>
-                <Field name="city" component="input" type="text" component={InputField} validate={isRequired} placeholder="Zipcode or City, State" />
-              </div>
-
-              <div>
-                <label htmlFor="email">Your Email*</label>
-                <Field name="email" component={InputField} validate={isRequired} type="email" />
-              </div>
-
-              <div>
-                <label htmlFor="name">Your Name*</label>
-                <Field name="name" component={InputField} validate={isRequired} type="text" />
-              </div>
-
-              <div>
-                <label htmlFor="phone">Your Phone*</label>
-                <Field name="phone" component={InputField} validate={isRequired} type="text" />
-              </div>
-
-              <div>
-                <label htmlFor="caseDate">Date Pet Last Seen*</label>
-                <Field name="caseDate" component={InputField} validate={isRequired} type="text" />
-              </div>
-
-              <div>
-                <label>Notes</label>
                 <div>
-                  <Field name="description" component="textarea" placeholder="Animal color, breed, gender..."/>
+                  <label>Case Type*</label>
+                  <div className="row">
+                    <label className="col s6"><Field name="caseType" component="input" type="radio" value="lost"/><span>Lost Animal</span></label>
+                    <label className="col s6"><Field name="caseType" component="input" type="radio" value="found"/><span>Found Animal</span></label>
+                  </div>
                 </div>
-              </div>
 
-                {
-                  isUpdate ?
-                      ''
-                      :
-                      (<div className="formCheckBox">
-                          <Field type="checkbox" component="input" name="terms" value="termsAccepted" checked/>
-                          <label> I accept and agree to the <Link to="#">Terms of Use</Link>.</label>
-                      </div>)
-                }
+                <div>
+                  <label>Animal Type*</label>
+                  <div className="row">
+                    <label className="col s4"><Field name="animalType" component="input" type="radio" value="cat"/><span>Cat</span></label>
+                    <label className="col s4"><Field name="animalType" component="input" type="radio" value="dog"/><span>Dog</span></label>
+                    <label className="col s4"><Field name="animalType" component="input" type="radio" value="other"/><span>Other</span></label>
+                  </div>
+                </div>
+
+                <div>
+                  <label>Animal Size*</label>
+                  <div className="row">
+                    <label className="col s4"><Field name="animalSize" component="input" type="radio" value="small"/><span>Small</span></label>
+                    <label className="col s4"><Field name="animalSize" component="input" type="radio" value="medium"/><span>Medium</span></label>
+                    <label className="col s4"><Field name="animalSize" component="input" type="radio" value="large"/><span>Large</span></label>
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="street">Location Pet Last Seen*</label>
+                  <Field name="street" component={InputField} validate={isRequired} type="text" placeholder="Street address or cross streets"/>
+                </div>
+
+                <div>
+                  <label htmlFor="city">City Pet Last Seen*</label>
+                  <Field name="city" component="input" type="text" component={InputField} validate={isRequired} placeholder="Zipcode or City, State" />
+                </div>
+
+                <div>
+                  <label htmlFor="email">Your Email*</label>
+                  <Field name="email" component={InputField} validate={isRequired} type="email" />
+                </div>
+
+                <div>
+                  <label htmlFor="name">Your Name*</label>
+                  <Field name="name" component={InputField} validate={isRequired} type="text" />
+                </div>
+
+                <div>
+                  <label htmlFor="phone">Your Phone*</label>
+                  <Field name="phone" component={InputField} validate={isRequired} type="text" />
+                </div>
+
+                <div>
+                  <label htmlFor="caseDate">Date Pet Last Seen*</label>
+                  <Field name="caseDate" component={InputField} validate={isRequired} type="text" />
+                </div>
+
+                <div>
+                  <label>Notes</label>
+                  <div>
+                    <Field name="description" component="textarea" placeholder="Animal color, breed, gender..."/>
+                  </div>
+                </div>
+
+                  {
+                    isUpdate ?
+                        ''
+                        :
+                        (<div className="formCheckBox">
+                            <Field type="checkbox" component="input" name="terms" value="termsAccepted" checked/>
+                            <label> I accept and agree to the <Link to="#">Terms of Use</Link>.</label>
+                        </div>)
+                  }
 
 
-            </form>
+              </form>
 
-          </div>
-        </main>
-          {
-            isUpdate ? (<div className="uploadSubmitBtnContainer">
-                <button className="waves-effect waves-light btn btn-action uploadSubmitBtn" form="uploadform" type="button" onClick={onReturn}>Go Back</button>
-                <button className="waves-effect waves-light btn btn-action uploadSubmitBtn" form="uploadform" type="submit">Submit</button>
-            </div>) : (<div className="uploadSubmitBtnContainer">
-                <button className="waves-effect waves-light btn btn-action uploadSubmitBtn" form="uploadform" type="submit">Submit</button>
-            </div>)
-          }
+            </div>
+          </main>
+            {
+              isUpdate ? (<div className="uploadSubmitBtnContainer">
+                  <button className="waves-effect waves-light btn btn-action uploadSubmitBtn" form="uploadform" type="button" onClick={onReturn}>Go Back</button>
+                  <button className="waves-effect waves-light btn btn-action uploadSubmitBtn" form="uploadform" type="submit">Submit</button>
+              </div>) : (<div className="uploadSubmitBtnContainer">
+                  <button className="waves-effect waves-light btn btn-action uploadSubmitBtn" form="uploadform" type="submit">Submit</button>
+              </div>)
+            }
 
-      </div>
-  )
+        </div>
+    );
+  }
 }
 
 UploadForm = reduxForm({
